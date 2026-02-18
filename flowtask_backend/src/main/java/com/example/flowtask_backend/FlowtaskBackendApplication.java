b3e0f78d7a04bd01d89c535cd8a1b51eacd169c0
migalo47 <miguelalonsogarc@gmail.com>
2026-02-18 17:23:35 +0100

    Primer commit: proyecto FlowTask

---
+++ flowtask_backend/src/main/java/com/example/flowtask_backend/FlowtaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowtask_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowtaskBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowtaskBackendApplication.class, args);
+    }
+
+}
---
+++ flowtask_backend/src/test/java/com/example/flowtask_backend/FlowtaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowtask_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowtaskBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
