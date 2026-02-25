5f410bd7e3e90db686dd2629df316c54b422b874
migalo47 <miguelalonsogarc@gmail.com>
2026-02-25 20:50:56 +0100

    Controllers y dto

--- flowtask_backend/src/main/java/com/example/flowtask_backend/controller/TaskController.java
+++ flowtask_backend/src/main/java/com/example/flowtask_backend/controller/TaskController.java
@@ -1,7 +1,20 @@
 package com.example.flowtask_backend.controller;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import com.example.flowtask_backend.service.TaskService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
+
+
 }
