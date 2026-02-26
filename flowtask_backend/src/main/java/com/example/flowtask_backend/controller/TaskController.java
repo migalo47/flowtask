a540ef051729df8d4717aa6f1953f609e70f170d
migalo47 <miguelalonsogarc@gmail.com>
2026-02-26 22:34:21 +0100

    TaskController y dto

--- flowtask_backend/src/main/java/com/example/flowtask_backend/controller/TaskController.java
+++ flowtask_backend/src/main/java/com/example/flowtask_backend/controller/TaskController.java
@@ -1,9 +1,13 @@
 package com.example.flowtask_backend.controller;
 
 
+import com.example.flowtask_backend.dto.TaskRequest;
+import com.example.flowtask_backend.entity.Task;
+import com.example.flowtask_backend.entity.User;
 import com.example.flowtask_backend.service.TaskService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -14,7 +18,44 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @PostMapping("")
+    public Task crearTask(@RequestBody TaskRequest request){
+        Task task = taskService.crearTask(
+                request.getTitle(), request.getDescription(), request.getStatus(), request.getUserId()
+        );
 
+        return task;
+    }
 
+    @GetMapping("")
+    public List<Task> obtenerTodasLasTareas(){
+        List<Task> tasks= taskService.obtenerTodasLasTareas();
+        return tasks;
+    }
+
+    @GetMapping("{id}")
+    public Task obtenerTareasPorId(@PathVariable Long id){
+        Task task= taskService.obtenerTareaPorId(id);
+        return task;
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Task>obtenerTareasPorUsuario(Long userId){
+
+        List<Task> tasks=taskService.obtenerTareasPorUsuario(userId);
+        return tasks;
+    }
+
+    @PutMapping("{id}")
+    public Task actualizarTask(@PathVariable Long id, @RequestBody TaskRequest request){
+        Task task=taskService.actualizarTask(id, request.getTitle(), request.getDescription(), request.getStatus());
+        return task;
+    }
+
+    @DeleteMapping("{id}")
+    public void eliminarTask(@PathVariable Long id){
+        taskService.eliminarTask(id);
+        return ;
+    }
 
 }
---
+++ flowtask_backend/src/main/java/com/example/flowtask_backend/dto/TaskRequest.java
@@ -0,0 +1,42 @@
+package com.example.flowtask_backend.dto;
+
+public class TaskRequest {
+
+    private String title;
+    private String description;
+    private String status;
+    private Long userId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+}
